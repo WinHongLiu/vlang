35524546928ada7618c718e66db654b4aabda32d
WinHong <35715318+WinHongLiu@users.noreply.github.com>
2021-08-08 10:43:53 +0800

    Add files via upload

---
+++ day 01/function.v
@@ -0,0 +1,12 @@
+//函数创建
+
+fn add(x int, y int) int{ 
+	return x + y
+}
+fn sub(x,y int) int{
+	return x -y
+}
+fn main(){
+	println(add(770,330))
+	println(sub(1000,500))
+}
---
+++ day 01/variable.v
@@ -0,0 +1,10 @@
+//变量
+
+fn main(){
+	name:='jack'
+	age:=20
+	large_number :=i64(123)
+	println(name)
+	println(age)
+	println(large_number)
+}
